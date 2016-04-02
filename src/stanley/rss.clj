56f077b916ffcaba7045cb180b15a1c8f61b4ccb
Clark Kampfe <clark@bellycard.com>
2016-04-02 00:50:40 -0500

    goddamn formatting

--- src/stanley/rss.clj
+++ src/stanley/rss.clj
@@ -12,21 +12,21 @@
          (.getTime))))
 
 (defn feed [post-titles
-                 html-post-names
-                 post-created-ats
-                 post-formatted-contents]
+            html-post-names
+            post-created-ats
+            post-formatted-contents]
 
-    (apply rs/channel-xml
-           (assoc {}
-                  :title "Clark Kampfe"
-                  :link "https://zeroclarkthirty.com"
-                  :description "Clark Kampfe - zeroclarkthirty.com")
-           (map #(assoc {}
-                        :title %1
-                        :link (str "https://zeroclarkthirty.com/" %2)
-                        :pubDate (str->Date %3 )
-                        :description %4)
-                post-titles
-                html-post-names
-                post-created-ats
-                post-formatted-contents)))
+  (apply rs/channel-xml
+         (assoc {}
+                :title "Clark Kampfe"
+                :link "https://zeroclarkthirty.com"
+                :description "Clark Kampfe - zeroclarkthirty.com")
+         (map #(assoc {}
+                      :title %1
+                      :link (str "https://zeroclarkthirty.com/" %2)
+                      :pubDate (str->Date %3)
+                      :description %4)
+              post-titles
+              html-post-names
+              post-created-ats
+              post-formatted-contents)))
